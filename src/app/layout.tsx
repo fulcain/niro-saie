5bb8f91bb6052aa16042c24333fbb9271941e1cc
dofxo <kargat504@gmail.com>
2024-12-12 11:23:15 +0330

    feat: add yekan and iransansNumber fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./globals.scss";
 
 export const metadata: Metadata = {
   title: "PDL | نیرو ساعی درخش",
@@ -18,21 +18,21 @@ export default function RootLayout({
         <link
           rel="apple-touch-icon"
           sizes="180x180"
-          href="/apple-touch-icon.png"
+          href="/images/apple-touch-icon.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="32x32"
-          href="/favicon-32x32.png"
+          href="/images/favicon-32x32.png"
         />
         <link
           rel="icon"
           type="image/png"
           sizes="16x16"
-          href="/favicon-16x16.png"
+          href="/images/favicon-16x16.png"
         />
-        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="manifest" href="/images/site.webmanifest" />
       </head>
       <body>{children}</body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-center text-3xl">نیرو ساعی درخش</h1>
-    </main>
-  );
+  return <main></main>;
 }

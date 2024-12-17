45c987a24c5eed4ddbdf2084ea816ec8f49d5b09
Majid Kargar <majidkargar01@gmail.com>
2024-12-17 15:25:33 +0330

    Feat: Add the text section in contactus

--- src/app/(landing)/(contact-us)/CallNow.tsx
+++ src/app/(landing)/(contact-us)/CallNow.tsx
@@ -4,14 +4,15 @@ import Image from "next/image";
 
 const CallNow = () => {
   return (
-    <div className="flex flex-col items-center justify-center relative call-now h-[230px] w-[300px] bg-[url('/images/contact-us/call-now-bg.png')] bg-cover bg-no-repeat bg-center rounded-[8px]">
+    <div className="flex flex-col p-2 items-center justify-center relative call-now h-[230px] w-[300px] bg-[url('/images/contact-us/call-now-bg.png')] bg-cover bg-no-repeat bg-center rounded-[8px]">
       <div className="z-[2] bg-palette-primary rounded-full">
         <Image
           src="/images/contact-us/phone-call.svg"
-          width={84}
-          height={84}
+          width={60}
+          height={60}
           alt="phone call"
-          className="p-5"
+          sizes="(max-width: 768px) 60px, 60px"
+          className="p-5 h-auto md:w-[84px] md:h-[84px]"
         />
       </div>
       <div className="z-[2] mt-3 flex flex-col items-center justify-center gap-2">
--- src/app/(landing)/(contact-us)/ContactUs.tsx
+++ src/app/(landing)/(contact-us)/ContactUs.tsx
@@ -3,32 +3,30 @@ import { phoneNumber } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 import "./_contact-us.scss";
-import { BsTelephoneOutboundFill } from "react-icons/bs";
 import LinkUnderLine from "@/components/link-underline/LinkUnderline";
+import CallNow from "./CallNow";
 
 const ContactUs = () => {
   return (
     <section id="contact-us" className="container">
       <SectionTitle title="راه ارتباط با ما" subTitle="ارتباط با ما" />
       {/* TODO: change image color */}
-      <div className="relative flex justify-between items-center p-3 mt-[60px] bg-[url('/images/contact-us/contact-us-bg.png')] bg-cover bg-no-repeat bg-center w-full h-[250px] rounded-[10px]">
-        <div className="flex flex-col items-center justify-center relative call-now h-[230px] w-[300px] bg-[url('/images/contact-us/call-now-bg.png')] bg-cover bg-no-repeat bg-center rounded-[8px]">
-          <div className="z-[2] bg-palette-primary rounded-full">
-            <Image
-              src="/images/contact-us/phone-call.svg"
-              width={84}
-              height={84}
-              alt="phone call"
-              className="p-5"
-            />
+      <div className="relative flex flex-col md:flex-row gap-10 items-center p-3 mt-[60px] bg-[url('/images/contact-us/contact-us-bg.png')] bg-cover bg-no-repeat bg-center w-full h-[250px] rounded-[10px]">
+        <CallNow />
+        <div className="flex flex-col gap-4 text-white">
+          <div className="flex flex-row gap-1 items-start justify-center text-[22px]">
+            <p>شرکت نیرو ساعی</p>{" "}
+            <Link
+              className="text-palette-primary"
+              href={`tel:${phoneNumber}`}
+            >
+              اکنون تماس بگیرید!
+            </Link>
           </div>
-          <div className="z-[2] mt-3 flex flex-col items-center justify-center gap-2">
-            <span className="text-[18px] font-bold">اکنون تماس بگیرید</span>
-            <LinkUnderLine text={phoneNumber} href={`tel:${phoneNumber}`} />
-          </div>
+          <p>ما می‌توانیم یک راه‌حل قابل‌اعتماد به شما ارائه دهیم.</p>
         </div>
         <Link
-          className="text-white absolute top-1/2 -translate-y-1/2 left-10 rotate-270 bg-palette-primary hover:bg-white hover:text-black p-2 rounded-[12px] transition"
+          className="hidden lg:block text-white absolute top-1/2 -translate-y-1/2 left-10 rotate-270 bg-palette-primary hover:bg-white hover:text-black p-2 rounded-[12px] transition"
           href={`tel:${phoneNumber}`}
         >
           تماس بگیرید

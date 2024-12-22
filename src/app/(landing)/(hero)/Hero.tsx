07b2d5851cd1da02335e6c820a03d6c4d0852f38
dofxo <kargat504@gmail.com>
2024-12-22 16:51:16 +0330

    refactor: change hero section image

--- src/app/(landing)/(hero)/Hero.tsx
+++ src/app/(landing)/(hero)/Hero.tsx
@@ -2,10 +2,12 @@ const Hero = () => {
   return (
     // TODO: consider using bg-fixed
     // change the bg image
-    <section className="bg-[url('/images/hero/hero-bg.svg')] h-[100vh] bg-no-repeat bg-center bg-cover">
-      <div className="container text-palette-primary flex items-center justify-center h-full">
-        <div className="flex flex-col">
-          <h1 className="text-[25px] md:text-[60px]">Power Diligence Luminosity</h1>
+    <section className="bg-[url('/images/hero/hero-bg.png')] h-[100vh] bg-no-repeat bg-center bg-cover">
+      <div className="container text-palette-primary flex justify-center h-full">
+        <div className="flex flex-col mt-[12%]">
+          <h1 className="text-[25px] md:text-[60px]">
+            Power Diligence Luminosity
+          </h1>
           <h2 className="text-[15px] md:text-[40px]">نیرو ساعی درخش</h2>
         </div>
       </div>

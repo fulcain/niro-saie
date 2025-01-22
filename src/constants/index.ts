b856b18761d0c78345127402183fdd9259ab1ce2
dofxo <kargat504@gmail.com>
2025-01-22 17:00:09 +0330

    feat: add address to header and footer

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,6 +1,8 @@
 // header
-export const phoneNumber = "۰۹۱۷۰۰۰۰۰۰۰";
+export const phoneNumber = "۰۹۳۳۷۴۱۴۹۳۴";
 
 export const instagram = "https://instagram.com/niro_saee";
 
 export const appName = "Niro Saie Darakhsh";
+
+export const address = "https://maps.app.goo.gl/DXU4qxdyDcgsPbJS6";
